42d27af65371d05dee039880cc58cd9986fbd5ea
Firman <firman.upwork@gmail.com>
2022-09-13 07:30:55 +0700

    update config standard format

--- config/blog.php
+++ config/blog.php
@@ -4,17 +4,17 @@ return [
     'name' => 'My Blog',
 
     'author' => [
-        'name'      => null,
+        'name' => null,
         'job_title' => null,
-        'bio'       => null,
+        'bio' => null,
     ],
 
     'post' => [
-        'per_page'  => 5,
+        'per_page' => 5,
     ],
 
     'latest' => [
-        'per_page'  => 5,
+        'per_page' => 5,
     ],
 
     'copyright' => 'Copyright © 2022 Firman.',
