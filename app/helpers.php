a6a715442a366b9a4861f3bb2398bea2cc0d6de5
Firman <firman.upwork@gmail.com>
2022-09-13 07:28:53 +0700

    update helper

--- app/helpers.php
+++ app/helpers.php
@@ -3,10 +3,10 @@
 use App\Models\Setting;
 
 if (! function_exists('setting')) {
-
     function setting($path)
     {
         $value = Setting::getByPath($path);
+
         return $value ?: config($path, null);
     }
 }
